fc0eb2da2885bc062c02b51464abef6f874d2536
Simon Mourier <simon_mourier@hotmail.com>
2023-05-09 10:38:56 +0200

    Added all effects properties' enumerated types.

--- WicNet/Properties/AssemblyVersionInfo.cs
+++ WicNet/Properties/AssemblyVersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.6.4.1")]
-[assembly: AssemblyFileVersion("1.6.4.1")]
-[assembly: AssemblyInformationalVersion("1.6.4.1")]
+[assembly: AssemblyVersion("1.6.5.0")]
+[assembly: AssemblyFileVersion("1.6.5.0")]
+[assembly: AssemblyInformationalVersion("1.6.5.0")]
