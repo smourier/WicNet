f6f3283b1ae97e8d1cc796d6e119cc7a58c7c7dd
Simon Mourier <simon_mourier@hotmail.com>
2023-11-07 06:55:54 +0100

    Update nuget conf.

--- WicNet/nugetpub.bat
+++ WicNet/nugetpub.bat
@@ -1,3 +1,4 @@
 rmdir nupkg /Q /S
+SET NUGET_ENABLE_LEGACY_CSPROJ_PACK=true
 nuget pack -Build -OutputDirectory nupkg -properties Configuration=Release
 nuget push nupkg\*.nupkg -Source https://api.nuget.org/v3/index.json
