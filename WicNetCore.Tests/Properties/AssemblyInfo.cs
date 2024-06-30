aa150967dde11abccc49af590cb6bccec0d8b14d
Simon Mourier <smourier@users.noreply.github.com>
2024-06-30 11:32:54 +0200

    Updated some dependencies.

---
+++ WicNetCore.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿#if DEBUG
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+[assembly: AssemblyTitle("WicNetCore.Tests")]
+[assembly: AssemblyDescription("WIC, Direct2D and DWrite interop code.")]
+[assembly: AssemblyCompany("Simon Mourier")]
+[assembly: AssemblyProduct("WicNet")]
+[assembly: AssemblyCopyright("Copyright (C) 2021-2024 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("31cdfd19-60ce-428e-b249-365a781e2ee5")]
+[assembly: DisableRuntimeMarshalling]
+[assembly: SupportedOSPlatform("windows8.0")]
+
