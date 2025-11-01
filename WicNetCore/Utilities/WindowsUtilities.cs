46ab67fecdf92bafb08ba1f6832a1080561f5cd4
Simon Mourier <simon_mourier@hotmail.com>
2025-11-01 13:14:27 +0100

    Updated packages

--- WicNetCore/Utilities/WindowsUtilities.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WicNet.Utilities;
-
-public static class WindowsUtilities
-{
-    private static readonly Lazy<Process> _currentProcess = new(Process.GetCurrentProcess, true);
-    public static Process CurrentProcess => _currentProcess.Value;
-}
